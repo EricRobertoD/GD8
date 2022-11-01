55ce5ba492b11cd76e3f38d1373c5f4bbf440d0c
Lepidophylla <ericroberto2322@gmail.com>
2022-11-01 08:25:38 +0700

    initial commit

--- app/src/main/java/com/example/gd8_camera_d_11007/MainActivity.kt
+++ app/src/main/java/com/example/gd8_camera_d_11007/MainActivity.kt
@@ -1,36 +1,71 @@
-package com.example.gd8_camera_d_11007
+package com.example.gd8_accelerometer_d_11007
 
-import android.annotation.SuppressLint
-import android.hardware.Camera
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.widget.FrameLayout
-import android.widget.ImageButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
 
-class MainActivity : AppCompatActivity() {
-    private var mCamera: Camera? = null
-    private var mCameraView: CameraView? = null
+class MainActivity : AppCompatActivity(), SensorEventListener {
+    private lateinit var sensorManager: SensorManager
+    private lateinit var square: TextView
 
+    // Keeps phone in light mode
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        try {
-            mCamera = Camera.open()
-        }catch (e: Exception){
-            Log.d("Error", "Failed to get Camera" + e.message)
-        }
-
-        if(mCamera!=null){
-            mCameraView = CameraView(this, mCamera!!)
-            val camera_view = findViewById<View>(R.id.FLCamera) as FrameLayout
-            camera_view.addView(mCameraView)
-        }
-
-        @SuppressLint("MissingInflatedId", "LocalSuppress") val imageClose =
-            findViewById<View>(R.id.imgClose) as ImageButton
-        imageClose.setOnClickListener{view: View? -> System.exit(0)}
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        square = findViewById(R.id.tv_square)
+        setUpSensorStuff()
     }
+
+    private fun setUpSensorStuff(){
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        // Specify the sensor you want to listen to
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also{ accelerometer ->
+            sensorManager.registerListener(
+                this,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_FASTEST,
+                SensorManager.SENSOR_DELAY_FASTEST
+            )
+        }
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        // Checks for the sensor we have registered
+        if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+            //Log.d("Main", "onSensorChanged: sides ${event.values[0]} front/back ${event.values[1]} ")
+            // Sides = Tilting phone left(10) and right(-10)
+            val sides = event.values[0]
+            // Up/Down = Tilting phone up(10), flat (0), upside-down(- 10)
+            val upDown = event.values[1]
+            square.apply {
+                rotationX = upDown * 3f
+                rotationY = sides * 3f
+                rotation = -sides
+                translationX = sides * -10
+                translationY = upDown * 10
+            }
+            // Changes the colour of the square if it's completely flat
+            val color = if (upDown.toInt() == 0 && sides.toInt() == 0)
+                Color.GREEN else Color.RED
+            square.setBackgroundColor(color)
+            square.text = "up/down ${upDown.toInt()}\nleft/right ${sides.toInt()} "
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        return
+    }
+
+    override fun onDestroy() {
+        sensorManager.unregisterListener(this)
+        super.onDestroy()
+    }
+
 }
